c3797860e915c7fef0560bab49d836f32c73cd4f
cyberpotato2077 <cyberpotato96@gmail.com>
2025-05-06 01:46:19 +0900

    프로바이더 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import Providers from './providers';
+import { OverlayProvider } from 'overlay-kit';
 import './globals.css';
 
 const geistSans = Geist({
@@ -26,7 +27,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Providers>{children}</Providers>
+        <OverlayProvider>
+          <Providers>{children}</Providers>
+        </OverlayProvider>
       </body>
     </html>
   );
