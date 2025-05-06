9f25718504aa49b7e5661e60f86065a198cb6648
cyberpotato2077 <cyberpotato96@gmail.com>
2025-05-06 20:09:14 +0900

    폼 최신화

--- app/hooks/useAuthModal/index.tsx
+++ app/hooks/useAuthModal/index.tsx
@@ -1,10 +1,10 @@
 import { overlay } from 'overlay-kit';
-import { SignInForm } from '@/components/signin-form';
 import { Dialog, DialogContent, DialogTitle } from '@/components/ui/dialog';
 import { VisuallyHidden } from '@radix-ui/react-visually-hidden';
-import { SignUpForm } from '@/components/signup-form';
 import { useState } from 'react';
 import { match } from 'ts-pattern';
+import { SignIn } from './sign-in';
+import { SignUp } from './sign-up';
 
 export function useAuthModal() {
   const openAuthModal = () => overlay.open(({ isOpen, close }) => <AuthModal isOpen={isOpen} closeModal={close} />);
@@ -21,8 +21,8 @@ export function AuthModal({ isOpen, closeModal }: { isOpen: boolean; closeModal:
           <DialogTitle>로그인/회원가입</DialogTitle>
         </VisuallyHidden>
         {match(signState)
-          .with('SIGN_IN', () => <SignInForm onSignUpClick={() => setSignState('SIGN_UP')} />)
-          .with('SIGN_UP', () => <SignUpForm onBackToSignInClick={() => setSignState('SIGN_IN')} />)
+          .with('SIGN_IN', () => <SignIn onSignUpClick={() => setSignState('SIGN_UP')} />)
+          .with('SIGN_UP', () => <SignUp onBackToSignInClick={() => setSignState('SIGN_IN')} />)
           .exhaustive()}
       </DialogContent>
     </Dialog>
