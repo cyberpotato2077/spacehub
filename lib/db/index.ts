5778489b6e88dd5fa71364db40921afd7932b711
cyberpotato2077 <cyberpotato96@gmail.com>
2025-05-06 22:09:22 +0900

    수정

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -3,6 +3,6 @@ import { invariant } from 'es-toolkit';
 
 export const sql = (() => {
   const postgresUrl = process.env.POSTGRES_URL;
-  invariant(postgresUrl != null, `연결할 DB 주소가 누락되었습니다. ${process.env.TEST}`);
+  invariant(postgresUrl != null, '연결할 DB 주소가 누락되었습니다.');
   return postgres(postgresUrl, { ssl: 'require' });
 })();
