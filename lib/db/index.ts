a427cfc45362783075a5dad4e2e9a41a8ef6e8f2
cyberpotato2077 <cyberpotato96@gmail.com>
2025-05-06 21:50:58 +0900

    .

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -3,6 +3,6 @@ import { invariant } from 'es-toolkit';
 
 export const sql = (() => {
   const postgresUrl = process.env.POSTGRES_URL;
-  invariant(postgresUrl != null, '연결할 DB 주소가 누락되었습니다.');
+  invariant(postgresUrl != null, `연결할 DB 주소가 누락되었습니다. ${process.env.TEST}`);
   return postgres(postgresUrl, { ssl: 'require' });
 })();
