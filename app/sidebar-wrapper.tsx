a4a58ef444d3059d34e4eb294aecfb3bc9aae1e5
cyberpotato2077 <cyberpotato96@gmail.com>
2025-05-10 02:36:51 +0900

    사이드바 타이틀 추가

--- app/sidebar-wrapper.tsx
+++ app/sidebar-wrapper.tsx
@@ -21,7 +21,7 @@ export function SidebarWrapper({ children }: { children: ReactNode }) {
     >
       <AppSidebar variant="inset" />
       <SidebarInset>
-        <SiteHeader title={contextPath} />
+        <SiteHeader title={contextPath != null ? (contextPath === '' ? 'HOME' : contextPath.toUpperCase()) : ''} />
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">{children}</div>
         </div>
