2173576d9251975de0df1d208dbcf22fde9e0db7
cyberpotato2077 <cyberpotato96@gmail.com>
2025-05-10 19:30:32 +0900

    articles

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,3 +1,18 @@
-export default function Articles() {
-  return <div>Articles</div>;
+// app/posts/page.tsx
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+import Articles from './articles';
+import { getArticlesQueryOptions } from '@/lib/api/getArticles';
+
+export default async function PostsPage() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery(getArticlesQueryOptions({}));
+
+  return (
+    // Neat! Serialization is now as easy as passing props.
+    // HydrationBoundary is a Client Component, so hydration will happen there.
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Articles />
+    </HydrationBoundary>
+  );
 }
