c222285a97356b447155cf1d018eced7e605ba46
cyberpotato2077 <cyberpotato96@gmail.com>
2025-05-11 21:47:10 +0900

    ui 추가

--- app/articles/articles.tsx
+++ app/articles/articles.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
+import { NewsCard } from '@/components/news-card';
 import { getArticlesQueryOptions } from '@/lib/api/getArticles';
 import { Routes } from '@/lib/routes';
 import { useSuspenseQuery } from '@tanstack/react-query';
@@ -11,17 +11,26 @@ export default function Articles() {
   const { data } = useSuspenseQuery(getArticlesQueryOptions({}));
 
   return (
-    <>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
       {data.results.map((result) => (
-        <div key={result.id}>
-          <Button
-            type="button"
-            onClick={() => router.push(Routes.article({ id: result.id }))}>
-            go
-          </Button>
-          {JSON.stringify(result)}
-        </div>
+        <NewsCard
+          key={result.id}
+          item={{
+            events: result.events,
+            featured: result.featured,
+            id: result.id,
+            image_url: result.image_url,
+            launches: result.launches,
+            news_site: result.news_site,
+            published_at: result.published_at,
+            summary: result.summary,
+            title: result.title,
+            updated_at: result.updated_at,
+            url: result.url,
+          }}
+          onClick={() => router.push(Routes.article({ id: result.id }))}
+        />
       ))}
-    </>
+    </div>
   );
 }
