2587813a969780f69c7a127e350becb8422cbf00
cyberpotato2077 <cyberpotato96@gmail.com>
2025-05-11 21:23:10 +0900

    fix: 라이브 환경에서 api route가 파라미터를 받지 못하는 문제를 수정 (#38)

--- app/api/features/reports/[id]/route.ts
+++ app/api/features/reports/[id]/route.ts
@@ -1,16 +1,13 @@
 import { type NextRequest, NextResponse } from 'next/server';
 
-export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url);
-  const id = searchParams.get('id');
+export async function GET(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
 
-  if (!id) {
+  if (id == null) {
     return NextResponse.json({ error: 'Missing id' }, { status: 400 });
   }
 
-  const response = await fetch(
-    `https://api.spaceflightnewsapi.net/v4/reports/${id}`
-  );
+  const response = await fetch(`https://api.spaceflightnewsapi.net/v4/reports/${id}`);
   const data = await response.json();
 
   return NextResponse.json(data);
